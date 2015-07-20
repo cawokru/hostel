0ca7d2095638de10db01825ad9650ccafc8610e5
cawokru <roussu.aleksander@gmail.com>
2015-07-21 00:14:40 +0300

    Hotels photos

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -35,6 +35,9 @@ Rails.application.configure do
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
 
-  #Needed for devise
+  # Needed for devise
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+
+  # Needed for paperclip
+  Paperclip.options[:command_path] = 'C:\RailsInstaller\ImageMagick'
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150720192036) do
+ActiveRecord::Schema.define(version: 20150720205135) do
 
   create_table "addresses", force: true do |t|
     t.string   "country"
@@ -33,6 +33,10 @@ ActiveRecord::Schema.define(version: 20150720192036) do
     t.integer  "address_id"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "photo_file_name"
+    t.string   "photo_content_type"
+    t.integer  "photo_file_size"
+    t.datetime "photo_updated_at"
   end
 
   add_index "hotels", ["address_id"], name: "index_hotels_on_address_id"
